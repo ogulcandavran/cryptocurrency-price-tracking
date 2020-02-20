33b9d6bb55c8e4e9e711f9c0c613d683173e28b1
ogulcandavran <ogulcandavran@yahoo.com>
2020-02-21 01:11:44 +0200

    Headings UI improvements

--- src/components/Headings.js
+++ src/components/Headings.js
@@ -1,9 +1,37 @@
-import React from 'react'
-import {connect} from 'react-redux';
-import {sortCoins} from '../actions';
+import React from "react";
+import { connect } from "react-redux";
+import { sortCoins } from "../actions";
 
-function Headings({title, ascParameterString, descParameterString, sortParamChange, sortParam}) {
-return <th onClick={()=> sortParamChange(sortParam === ascParameterString ? descParameterString : ascParameterString)}>{title}</th>
+function Headings({
+  title,
+  ascParameterString,
+  descParameterString,
+  sortParamChange,
+  sortParam
+}) {
+  const showSortingDirection = () => {
+    if (sortParam === ascParameterString) {
+      return <span>a</span>;
+    } else if (sortParam === descParameterString) {
+      return <span>d</span>;
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <th
+      onClick={() =>
+        sortParamChange(
+          sortParam === ascParameterString
+            ? descParameterString
+            : ascParameterString
+        )
+      }
+    >
+      {title} {showSortingDirection()}
+    </th>
+  );
 }
 
-export default connect(null,{sortCoins})(React.memo(Headings));
+export default connect(null, { sortCoins })(React.memo(Headings));
